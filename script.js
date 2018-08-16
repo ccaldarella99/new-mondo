69a8e9d7e156a2ea9dd8b3807e3d6c869a1793a5
ccaldarella99 <christopher.caldarella@outlook.com>
2018-08-16 15:36:20 -0600

    Inject Racks into HTML page with JS (removed HTML racks from HTML page)

--- script.js
+++ script.js
@@ -2,8 +2,54 @@
 
 var dataLength = myData.length;
 
+document.getElementById("container").innerHTML = "";
 for(var i=0; i< dataLength; i++)
 {
+	alert(i);
+	var j = (Math.floor(i/2)+1);
+	var fb = "";
+	if(i % 2 == 0)
+	{ fb = "f"; }
+	else
+	{ fb = "b"; }
+
+	var addRack =
+	`		<div class="rack-group">
+			<span class="name" id="r` + j + `-` + fb + `-name"></span>
+			<div class="rack front" id="rack-one-` + fb +  `">
+				<div class="slot s1" id="r` + j + `-s1-` + fb +  `">
+					<span class="slot-text" id="r` + j + `-s1-` + fb +  `-text"></span>
+				</div>
+				<div class="slot s2" id="r` + j + `-s2-` + fb +  `">
+					<span class="slot-text" id="r` + j + `-s2-` + fb +  `-text"></span>
+				</div>
+				<div class="slot s3" id="r` + j + `-s3-` + fb +  `">
+					<span class="slot-text" id="r` + j + `-s3-` + fb +  `-text"></span>
+				</div>
+				<div class="slot s4" id="r` + j + `-s4-` + fb +  `">
+					<span class="slot-text" id="r` + j + `-s4-` + fb +  `-text"></span>
+				</div>
+				<div class="tmc" id="r` + j + `-tmc-` + fb +  `">&nbsp;
+					<span class="tmc-type" id="r` + j + `-tmc-` + fb +  `-type"></span>
+					<span class="tmc-text" id="r` + j + `-tmc-` + fb +  `-text"></span>
+				</div>
+				<div class="slot s5" id="r` + j + `-s5-` + fb +  `">
+					<span class="slot-text" id="r` + j + `-s5-` + fb +  `-text"></span>
+				</div>
+				<div class="slot s6" id="r` + j + `-s6-` + fb +  `">
+					<span class="slot-text" id="r` + j + `-s6-` + fb +  `-text"></span>
+				</div>
+				<div class="slot s7" id="r` + j + `-s7-` + fb +  `">
+					<span class="slot-text" id="r` + j + `-s7-` + fb +  `-text"></span>
+				</div>
+				<div class="slot s8" id="r` + j + `-s8-` + fb +  `">
+					<span class="slot-text" id="r` + j + `-s8-` + fb +  `-text"></span>
+				</div>
+			</div>
+		</div>`;
+		
+	document.getElementById("container").innerHTML += addRack;
+
 	document.getElementById(myData[i].id + "-name").innerHTML = myData[i].Name + " - " + myData[i].Position + "<br /><br />";
 	document.getElementById(myData[i].Slot1.id + "-text").innerHTML = "Manager: " + myData[i].Slot1.Manager + "<br />Date: " + myData[i].Slot1.Date + "<br />HE: " + myData[i].Slot1.HE;
 	document.getElementById(myData[i].Slot2.id + "-text").innerHTML = "Manager: " + myData[i].Slot2.Manager + "<br />Date: " + myData[i].Slot2.Date + "<br />HE: " + myData[i].Slot2.HE;
