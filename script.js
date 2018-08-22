a56ee96ff12b1103700e9d2903b26a069f1ac961
ccaldarella99 <christopher.caldarella@outlook.com>
2018-08-22 15:24:42 -0600

    Overlay pulls data for all Slots, need TMC, still cannot save

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function loadData() {
 		{ fb = "b"; }
 
 		var addRack =
-	`		<div class="rack-group">
+	`		<div class="rack-group ` + fb + `">
 				<span class="name" id="r` + j + `-` + fb + `-name"></span>
 				<div class="rack front" id="rack-one-` + fb +  `">
 					<div class="slot s1" id="r` + j + `-s1-` + fb +  `">
@@ -51,7 +51,7 @@ function loadData() {
 					</div>
 				</div>
 				<div>
-					<i class="fa fa-edit rack-edit" id="edit-r` + j + `-` + fb +  `" onclick="editRack('` + j + `','` + fb +  `')"></i>
+					<i class="fa fa-edit rack-edit" id="edit-r` + j + `-` + fb +  `" onclick="editRack('` + i + `','` + fb +  `')"></i>
 				</div>
 			</div>`;
 
@@ -84,54 +84,164 @@ function loadData() {
 		document.getElementById(myData[i].TMCBOT.id + "-type").innerHTML = myData[i].TMCBOT.Type;
 	}
 
-	//return myData;
+	return myData;
 }
 
-function editRack(j, fb, slot) {
-	//alert("NAME: " + myData[j].Name);
-	//alert("POSITION: " + myData[j].Position);
-	//alert("For editing: r" + j + "-" + fb);
-		var addRack =
-	`			NAME: <input type="text" id="r` + j + `-` + fb + `-name-form" value="no"><br />
-				Position: <input type="text" id="r` + j + `-` + fb + `-position"><br />
-				SLOT ` + slot + `:<br />
-				Manager: <input id="r` + j + `-s` + slot + `-` + fb +  `-text-mgr"><br />
-				Date: <input id="r` + j + `-s` + slot + `-` + fb +  `-text-date"><br />
-				HE: <input id="r` + j + `-s` + slot + `-` + fb +  `-text-he">`;
+function editRack(i, fb) {
+	var j = (Math.floor(i/2) + 1);
+	var addRack =
+	`<table class="t-pos">
+<tr><th><h2>NAME:</h2></th><th><input type="text" id="r` + j + `-` + fb + `-name-form"></th></tr>
+<tr><th><h2>Position:</h2></th><th><input type="text" id="r` + j + `-` + fb + `-position"></th></tr>
+
+<tr><th colspan="2" class="t-slot-head"   >SLOT 1:</th></tr>
+<tr><td>Manager:</td><td><input id="r` + j + `-s1-` + fb +  `-text-mgr"></td></tr>
+<tr><td>Date:</td><td><input id="r`    + j + `-s1-` + fb +  `-text-date"></td></tr>
+<tr><td>HE:</td><td><input id="r` +      j + `-s1-` + fb +  `-text-he"></td></tr>
+
+<tr><th colspan="2" class="t-slot-head"   >SLOT 2:</th></tr>
+<tr><td>Manager:</td><td><input id="r` + j + `-s2-` + fb +  `-text-mgr"></td></tr>
+<tr><td>Date:</td><td><input id="r`    + j + `-s2-` + fb +  `-text-date"></td></tr>
+<tr><td>HE:</td><td><input id="r` +      j + `-s2-` + fb +  `-text-he"></td></tr>
+
+<tr><th colspan="2" class="t-slot-head"   >SLOT 3:</th></tr>
+<tr><td>Manager:</td><td><input id="r` + j + `-s3-` + fb +  `-text-mgr"></td></tr>
+<tr><td>Date:</td><td><input id="r`    + j + `-s3-` + fb +  `-text-date"></td></tr>
+<tr><td>HE:</td><td><input id="r` +      j + `-s3-` + fb +  `-text-he"></td></tr>
+
+<tr><th colspan="2" class="t-slot-head"   >SLOT 4:</th></tr>
+<tr><td>Manager:</td><td><input id="r` + j + `-s4-` + fb +  `-text-mgr"></td></tr>
+<tr><td>Date:</td><td><input id="r`    + j + `-s4-` + fb +  `-text-date"></td></tr>
+<tr><td>HE:</td><td><input id="r` +      j + `-s4-` + fb +  `-text-he"></td></tr>
+
+<tr><th colspan="2" class="t-slot-head"   >SLOT 5:</th></tr>
+<tr><td>Manager:</td><td><input id="r` + j + `-s5-` + fb +  `-text-mgr"></td></tr>
+<tr><td>Date:</td><td><input id="r`    + j + `-s5-` + fb +  `-text-date"></td></tr>
+<tr><td>HE:</td><td><input id="r` +      j + `-s5-` + fb +  `-text-he"></td></tr>
+
+<tr><th colspan="2" class="t-slot-head"   >SLOT 6:</th></tr>
+<tr><td>Manager:</td><td><input id="r` + j + `-s6-` + fb +  `-text-mgr"></td></tr>
+<tr><td>Date:</td><td><input id="r`    + j + `-s6-` + fb +  `-text-date"></td></tr>
+<tr><td>HE:</td><td><input id="r` +      j + `-s6-` + fb +  `-text-he"></td></tr>
+
+<tr><th colspan="2" class="t-slot-head"   >SLOT 7:</th></tr>
+<tr><td>Manager:</td><td><input id="r` + j + `-s7-` + fb +  `-text-mgr"></td></tr>
+<tr><td>Date:</td><td><input id="r`    + j + `-s7-` + fb +  `-text-date"></td></tr>
+<tr><td>HE:</td><td><input id="r` +      j + `-s7-` + fb +  `-text-he"></td></tr>
+
+<tr><th colspan="2" class="t-slot-head"   >SLOT 8:</th></tr>
+<tr><td>Manager:</td><td><input id="r` + j + `-s8-` + fb +  `-text-mgr"></td></tr>
+<tr><td>Date:</td><td><input id="r`    + j + `-s8-` + fb +  `-text-date"></td></tr>
+<tr><td>HE:</td><td><input id="r` +      j + `-s8-` + fb +  `-text-he"></td></tr>
+
+<tr><td><div id="cancel" class="save-btn" onclick="closeOverlay();">Cancel</div></td><td><a download="info.txt" id="downloadlink" class="save-btn">Save & Create File</a></td></tr>
+</table>`;
 
 		document.getElementById("edit-rack").innerHTML = addRack;
-		//alert(document.getElementById(myData[j].id));
 
-		document.getElementById(myData[j].id + "-name-form").value = myData[j].Name;
-		document.getElementById(myData[j].id + "-position").value = myData[j].Position;
-		document.getElementById(myData[j].Slot1.id + "-text-mgr").value = myData[j].Slot1.Manager;
-		document.getElementById(myData[j].Slot1.id + "-text-date").value = myData[j].Slot1.Date;
-		document.getElementById(myData[j].Slot1.id + "-text-he").value = myData[j].Slot1.HE;
-		/*document.getElementById(myData[i].Slot2.id + "-text-form").innerHTML = "Manager: " + myData[i].Slot2.Manager + "<br />Date: " + myData[i].Slot2.Date + "<br />HE: " + myData[i].Slot2.HE;
-		document.getElementById(myData[i].Slot3.id + "-text").innerHTML = "Manager: " + myData[i].Slot3.Manager + "<br />Date: " + myData[i].Slot3.Date + "<br />HE: " + myData[i].Slot3.HE;
-		document.getElementById(myData[i].Slot4.id + "-text").innerHTML = "Manager: " + myData[i].Slot4.Manager + "<br />Date: " + myData[i].Slot4.Date + "<br />HE: " + myData[i].Slot4.HE;
-		document.getElementById(myData[i].Slot5.id + "-text").innerHTML = "Manager: " + myData[i].Slot5.Manager + "<br />Date: " + myData[i].Slot5.Date + "<br />HE: " + myData[i].Slot5.HE;
-		document.getElementById(myData[i].Slot6.id + "-text").innerHTML = "Manager: " + myData[i].Slot6.Manager + "<br />Date: " + myData[i].Slot6.Date + "<br />HE: " + myData[i].Slot6.HE;
-		document.getElementById(myData[i].Slot7.id + "-text").innerHTML = "Manager: " + myData[i].Slot7.Manager + "<br />Date: " + myData[i].Slot7.Date + "<br />HE: " + myData[i].Slot7.HE;
-		document.getElementById(myData[i].Slot8.id + "-text").innerHTML = "Manager: " + myData[i].Slot8.Manager + "<br />Date: " + myData[i].Slot8.Date + "<br />HE: " + myData[i].Slot8.HE;
-		//document.getElementById(myData[i].TMC.id + "-text").innerHTML = "IP: " + myData[i].TMC.IP + "<br />Name: " + myData[i].TMC.Name + "<br />" + myData[i].TMC.Number;
-		//document.getElementById(myData[i].TMC.id + "-type").innerHTML = myData[i].TMC.Type;
-		document.getElementById(myData[i].TMCTOP.id + "-text").innerHTML = "IP: " + myData[i].TMCTOP.IP + "<br />Name: " + myData[i].TMCTOP.Name + "<br />" + myData[i].TMCTOP.Number;
-		document.getElementById(myData[i].TMCTOP.id + "-type").innerHTML = myData[i].TMCTOP.Type;
-		document.getElementById(myData[i].TMCBOT.id + "-text").innerHTML = "IP: " + myData[i].TMCBOT.IP + "<br />Name: " + myData[i].TMCBOT.Name + "<br />" + myData[i].TMCBOT.Number;
-		document.getElementById(myData[i].TMCBOT.id + "-type").innerHTML = myData[i].TMCBOT.Type;//*/
+		//console.log("j=" + j);
+		//console.log("NAME: " + myData[i].Name);
+		//console.log("POS:  " + myData[i].Position);
+		document.getElementById(myData[i].id + "-name-form").value = myData[i].Name;
+		document.getElementById(myData[i].id + "-position").value = myData[i].Position;
 
-    document.getElementById("OverlayEdit").style.width = "100%";
+		document.getElementById(myData[i].Slot1.id + "-text-mgr").value =  myData[i].Slot1.Manager;
+		document.getElementById(myData[i].Slot1.id + "-text-date").value = myData[i].Slot1.Date;
+		document.getElementById(myData[i].Slot1.id + "-text-he").value =   myData[i].Slot1.HE;
+
+		document.getElementById(myData[i].Slot2.id + "-text-mgr").value =  myData[i].Slot2.Manager;
+		document.getElementById(myData[i].Slot2.id + "-text-date").value = myData[i].Slot2.Date;
+		document.getElementById(myData[i].Slot2.id + "-text-he").value =   myData[i].Slot2.HE;
+
+		document.getElementById(myData[i].Slot3.id + "-text-mgr").value =  myData[i].Slot3.Manager;
+		document.getElementById(myData[i].Slot3.id + "-text-date").value = myData[i].Slot3.Date;
+		document.getElementById(myData[i].Slot3.id + "-text-he").value =   myData[i].Slot3.HE;
+
+		document.getElementById(myData[i].Slot4.id + "-text-mgr").value =  myData[i].Slot4.Manager;
+		document.getElementById(myData[i].Slot4.id + "-text-date").value = myData[i].Slot4.Date;
+		document.getElementById(myData[i].Slot4.id + "-text-he").value =   myData[i].Slot4.HE;
+
+		document.getElementById(myData[i].Slot5.id + "-text-mgr").value =  myData[i].Slot5.Manager;
+		document.getElementById(myData[i].Slot5.id + "-text-date").value = myData[i].Slot5.Date;
+		document.getElementById(myData[i].Slot5.id + "-text-he").value =   myData[i].Slot5.HE;
+
+		document.getElementById(myData[i].Slot6.id + "-text-mgr").value =  myData[i].Slot6.Manager;
+		document.getElementById(myData[i].Slot6.id + "-text-date").value = myData[i].Slot6.Date;
+		document.getElementById(myData[i].Slot6.id + "-text-he").value =   myData[i].Slot6.HE;
+
+		document.getElementById(myData[i].Slot7.id + "-text-mgr").value =  myData[i].Slot7.Manager;
+		document.getElementById(myData[i].Slot7.id + "-text-date").value = myData[i].Slot7.Date;
+		document.getElementById(myData[i].Slot7.id + "-text-he").value =   myData[i].Slot7.HE;
+
+		document.getElementById(myData[i].Slot8.id + "-text-mgr").value =  myData[i].Slot8.Manager;
+		document.getElementById(myData[i].Slot8.id + "-text-date").value = myData[i].Slot8.Date;
+		document.getElementById(myData[i].Slot8.id + "-text-he").value =   myData[i].Slot8.HE;
+
+	openOverlay();
 }
 
 function addServer()  {
 	//alert("For Adding a new Mondo");
-    document.getElementById("OverlayEdit").style.width = "100%";
+	
+	var j = "x";
+	var fb = "y";
+	
+	var addRack =
+	`<table class="t-pos">
+<tr><th><h2>NAME:</h2></th><th><input type="text" id="r` + j + `-` + fb + `-name-form"></th></tr>
+<tr><th><h2>Position:</h2></th><th><input type="text" id="r` + j + `-` + fb + `-position"></th></tr>
+
+<tr><th colspan="2" class="t-slot-head"   >SLOT 1:</th></tr>
+<tr><td>Manager:</td><td><input id="r` + j + `-s1-` + fb +  `-text-mgr"></td></tr>
+<tr><td>Date:</td><td><input id="r`    + j + `-s1-` + fb +  `-text-date"></td></tr>
+<tr><td>HE:</td><td><input id="r` +      j + `-s1-` + fb +  `-text-he"></td></tr>
+
+<tr><th colspan="2" class="t-slot-head"   >SLOT 2:</th></tr>
+<tr><td>Manager:</td><td><input id="r` + j + `-s2-` + fb +  `-text-mgr"></td></tr>
+<tr><td>Date:</td><td><input id="r`    + j + `-s2-` + fb +  `-text-date"></td></tr>
+<tr><td>HE:</td><td><input id="r` +      j + `-s2-` + fb +  `-text-he"></td></tr>
+
+<tr><th colspan="2" class="t-slot-head"   >SLOT 3:</th></tr>
+<tr><td>Manager:</td><td><input id="r` + j + `-s3-` + fb +  `-text-mgr"></td></tr>
+<tr><td>Date:</td><td><input id="r`    + j + `-s3-` + fb +  `-text-date"></td></tr>
+<tr><td>HE:</td><td><input id="r` +      j + `-s3-` + fb +  `-text-he"></td></tr>
+
+<tr><th colspan="2" class="t-slot-head"   >SLOT 4:</th></tr>
+<tr><td>Manager:</td><td><input id="r` + j + `-s4-` + fb +  `-text-mgr"></td></tr>
+<tr><td>Date:</td><td><input id="r`    + j + `-s4-` + fb +  `-text-date"></td></tr>
+<tr><td>HE:</td><td><input id="r` +      j + `-s4-` + fb +  `-text-he"></td></tr>
+
+<tr><th colspan="2" class="t-slot-head"   >SLOT 5:</th></tr>
+<tr><td>Manager:</td><td><input id="r` + j + `-s5-` + fb +  `-text-mgr"></td></tr>
+<tr><td>Date:</td><td><input id="r`    + j + `-s5-` + fb +  `-text-date"></td></tr>
+<tr><td>HE:</td><td><input id="r` +      j + `-s5-` + fb +  `-text-he"></td></tr>
+
+<tr><th colspan="2" class="t-slot-head"   >SLOT 6:</th></tr>
+<tr><td>Manager:</td><td><input id="r` + j + `-s6-` + fb +  `-text-mgr"></td></tr>
+<tr><td>Date:</td><td><input id="r`    + j + `-s6-` + fb +  `-text-date"></td></tr>
+<tr><td>HE:</td><td><input id="r` +      j + `-s6-` + fb +  `-text-he"></td></tr>
+
+<tr><th colspan="2" class="t-slot-head"   >SLOT 7:</th></tr>
+<tr><td>Manager:</td><td><input id="r` + j + `-s7-` + fb +  `-text-mgr"></td></tr>
+<tr><td>Date:</td><td><input id="r`    + j + `-s7-` + fb +  `-text-date"></td></tr>
+<tr><td>HE:</td><td><input id="r` +      j + `-s7-` + fb +  `-text-he"></td></tr>
+
+<tr><th colspan="2" class="t-slot-head"   >SLOT 8:</th></tr>
+<tr><td>Manager:</td><td><input id="r` + j + `-s8-` + fb +  `-text-mgr"></td></tr>
+<tr><td>Date:</td><td><input id="r`    + j + `-s8-` + fb +  `-text-date"></td></tr>
+<tr><td>HE:</td><td><input id="r` +      j + `-s8-` + fb +  `-text-he"></td></tr>
+
+<tr><td><div id="cancel" class="save-btn" onclick="closeOverlay();">Cancel</div></td><td><a download="data1.js" id="downloadlink" class="save-btn" href="makeTextFile('r` + j + `-` + fb + `-name-form')">Save & Create File</a></td></tr>
+</table>`;
+
+		document.getElementById("edit-rack").innerHTML = addRack;
+
+	
+	openOverlay();
 }
 
 
-//var newData = 
-loadData();
+var newData = loadData();
 
 
 var blankObject = {
@@ -214,13 +324,44 @@ var blankObject = {
 
 
 
-//function openOverlay() {
-//    document.getElementById("OverlayEdit").style.width = "100%";
-//}
+function openOverlay() {
+    document.getElementById("OverlayEdit").style.width = "100%";
+}
 
 function closeOverlay() {
     document.getElementById("OverlayEdit").style.width = "0%";
 }
 
 
+function objectDataToText(txt){
+	return document.getElementById(txt).value;
+}
+
+(function () {
+var textFile = null,
+  makeTextFile = function (text) {
+    var data = new Blob([text], {type: 'text/plain'});
+
+    // If we are replacing a previously generated file we need to
+    // manually revoke the object URL to avoid memory leaks.
+    if (textFile !== null) {
+      window.URL.revokeObjectURL(textFile);
+    }
+
+    textFile = window.URL.createObjectURL(data);
+
+    return textFile;
+  };
+
+
+  var create = document.getElementById('downloadlink'),
+    textbox = document.getElementById('r1-f-name');
+    //textbox = document.getElementById('textbox');
+
+  create.addEventListener('click', function () {
+    var link = document.getElementById('downloadlink');
+    link.href = makeTextFile(textbox.value);
+    link.style.display = 'block';
+  }, false);
+})();
 
